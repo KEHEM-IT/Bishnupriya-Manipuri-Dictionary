5d902d78f9d9b7a391029a3eac8dbb31327a12ec
Surajit Singha Sisir <sci.surajit@gmail.com>
2026-01-09 03:48:24 +0600

    ADD TOOLTIP FOR MEANING
    Signed-off-by: Surajit Singha Sisir <sci.surajit@gmail.com>

--- frontend/src/views/WordDetail.vue
+++ frontend/src/views/WordDetail.vue
@@ -147,15 +147,20 @@ onMounted(async () => {
                         </h2>
                     </span>
                 </div>
+
                 <hr class="border-t border-gray-200 dark:border-gray-700">
+
                 <!-- MEANINGS -->
-                <div class="flex items-center gap-2 text-xl pt-4 pb-4 flex-wrap text-[#474747] dark:text-[#d8d8d8]">
-                     <i class="fas fa-arrows-turn-right text-[#868686]"></i>
+                <div class="w-max flex items-center gap-2 text-xl mt-4 pb-4 flex-wrap text-[#474747] dark:text-[#d8d8d8]"
+                    :data-tooltip="`${word.bpy} - ওৱাইর অর্থ`">
+                    <i class="fas fa-arrows-turn-right text-[#868686]"></i>
                     <span>{{ word.meaning?.join(", ") }}</span>
                 </div>
                 <!-- Description -->
-                <div class="flex gap-6 mb-4 flex-wrap text-[#474747] dark:text-[#d8d8d8] bg-black/5 dark:bg-white/5 border-2 rounded-lg  dark:border-white/5">
-                    <span class="px-4 py-2" :data-tooltip="`${word.bpy} - ওৱাইর হুরকাঙ ব্যাখ্যা আহান`">{{ displayedDescription }}</span>
+                <div
+                    class="flex gap-6 mb-4 flex-wrap text-[#474747] dark:text-[#d8d8d8] bg-black/5 dark:bg-white/5 border-2 rounded-lg  dark:border-white/5">
+                    <span class="px-4 py-2" :data-tooltip="`${word.bpy} - ওৱাইর হুরকাঙ ব্যাখ্যা আহান`">{{
+                        displayedDescription }}</span>
                     <button v-if="shouldShowSeeMore" @click="isExpanded = !isExpanded" class=" text-nowrap text-blue-500 hover:text-blue-600 dark:text-blue-400 dark:hover:text-blue-300 font-medium
                         transition-colors whitespace-nowrap">
                         {{ isExpanded ? 'See less' : 'See more' }}
@@ -334,7 +339,7 @@ onMounted(async () => {
                     <div v-if="word.grammar.gender" class="bg-green-50 dark:bg-green-900/20 p-6 rounded-xl">
                         <p class="text-sm text-gray-600 dark:text-gray-400 mb-1">Gender</p>
                         <p class="text-xl font-semibold text-gray-900 dark:text-white capitalize">{{ word.grammar.gender
-                            }}</p>
+                        }}</p>
                     </div>
                     <div v-if="word.grammar.number?.plural" class="bg-blue-50 dark:bg-blue-900/20 p-6 rounded-xl">
                         <p class="text-sm text-gray-600 dark:text-gray-400 mb-1">Plural Form</p>
